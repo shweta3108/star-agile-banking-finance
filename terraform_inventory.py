fabf69fa31533098a3a0e35f501fabf05520fa1a
sejalmm06 <129971092+sejalmm06@users.noreply.github.com>
2023-06-20 20:35:58 +0530

    Update terraform_inventory.py

--- terraform_inventory.py
+++ terraform_inventory.py
@@ -6,14 +6,11 @@ import subprocess
 output = subprocess.check_output(["terraform", "output"]).decode("utf-8")
 output_lines = output.splitlines()
 
-# Extract the IP addresses from the output
-test_server_ip = output_lines[0].split(" = ")[1].strip('"')
+# Extract the IP address of the production server from the output
 production_server_ip = output_lines[1].split(" = ")[1].strip('"')
 
 # Generate the inventory file
 with open("servers_inventory", "w") as f:
-    f.write("[testservers]\n")
-    f.write(test_server_ip + "\n\n")
     f.write("[prodservers]\n")
     f.write(production_server_ip + "\n\n")
     f.write("[webservers:vars]\n")
