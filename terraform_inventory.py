a7eb6e863b7521a99327743049949ff9a05ef7ce
sejalmm06 <129971092+sejalmm06@users.noreply.github.com>
2023-06-21 14:05:14 +0530

    Update terraform_inventory.py

--- terraform_inventory.py
+++ terraform_inventory.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 import subprocess
+import json
 
 # Run Terraform output command to get the IP addresses
-output = subprocess.check_output(["terraform", "output"]).decode("utf-8")
-output_lines = output.splitlines()
+output = subprocess.check_output(["terraform", "output", "-json"]).decode("utf-8")
+output_json = json.loads(output)
 
 # Extract the IP address of the production server from the output
-production_server_ip = output_lines[1].split(" = ")[1].strip('"')
+production_server_ip = output_json.get("production_server_ip", {}).get("value")
 
-# Generate the inventory file
-with open("servers_inventory", "w") as f:
-    f.write("[prodservers]\n")
-    f.write(production_server_ip + "\n\n")
-    f.write("[webservers:vars]\n")
-    f.write("ansible_ssh_user=ubuntu")
-
-# Print the inventory file content
-with open("servers_inventory", "r") as f:
-    print(f.read())
+# Generate the inventory file if the IP address is available
+if production_server_ip:
+    with open("servers_inventory", "w") as f:
+        f.write("[prodservers]\n")
+        f.write(production_server_ip + "\n\n")
+        f.write("[webservers:vars]\n")
+        f.write("ansible_ssh_user=ubuntu")
+        print("Inventory file generated successfully.")
+else:
+    print("Failed to extract the production server IP address from the Terraform output.")
