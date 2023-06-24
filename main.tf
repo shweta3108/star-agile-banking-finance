a6abe11a93c83490cd4888ae194f9fac8b78b0a5
shweta3108 <60587685+shweta3108@users.noreply.github.com>
2023-06-24 23:35:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "Prod-Server" {
+ ami = "ami-0261755bbcb8c4a84"
+ instance_type = "t2.micro"
+ availability_zone = "us-east-1"
+ key_name = "devops"
+
+tags ={
+name=" Terraform prod"
+}
+user_data = <<-EOF
+ #!/bin/bash
+ sudo apt-get update -y
+ sudo apt install docker.io -y
+ sudo systemctl enable docker
+ sudo docker run -itd -p 8085:8081 shwetas27/finance-me:1.0
+ sudo docker start $(docker ps -aq)
+EOF
+}
+ 
