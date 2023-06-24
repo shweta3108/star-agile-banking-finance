3e6a1bbab314d756897bcedcb979e1352499f23a
shweta3108 <60587685+shweta3108@users.noreply.github.com>
2023-06-24 11:52:24 +0530

    Update prod.tf

--- prod.tf
+++ prod.tf
@@ -3,18 +3,9 @@ resource "aws_instance" "Prod-Server" {
  instance_type = "t2.micro"
  availability_zone = "us-east-1"
  key_name = "devops"
- network_interface {
- device_index = 0
- network_interface_id = aws_network_interface.proj-ni.id
- }
- user_data  = <<-EOF
- #!/bin/bash
-     sudo apt-get update -y
-     sudo apt install docker.io -y
-     sudo systemctl enable docker
-     sudo docker run -itd -p 8085:8081 shwetas27/finance-me:1.0
-     sudo docker start $(docker ps -aq)
- EOF
- tags = {
- Name = "Prod-Server"
- }
+ sudo apt-get update -y
+ sudo apt install docker.io -y
+ sudo systemctl enable docker
+ sudo docker run -itd -p 8085:8081 shwetas27/finance-me:1.0
+ sudo docker start $(docker ps -aq)
+ 
