051864062f382908c6bbe6becb4c41a8dcaaf6d7
shweta3108 <60587685+shweta3108@users.noreply.github.com>
2023-06-25 13:19:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-
 resource "aws_instance" "Prod-Server" {
  ami = "ami-0261755bbcb8c4a84"
  instance_type = "t2.micro"
  availability_zone = "us-east-1a"
  key_name = "devops"
  vpc_security_group_ids = "sg-059e816842c060dc4"
-tags ={
-name=" Terraform prod"
-}
 
-user data = <<-EOF
-#!/bin/bash
+
+user data = <<EOF
+  #!/bin/bash
   apt-get update -y
   apt install docker.io -y
   systemctl enable docker
   docker run -itd -p 8087:8081 shwetas27/finance-me:1.0
   docker start $(docker ps -aq)
 EOF
+tags ={
+      name=" Terraform prod"
+}
 }
