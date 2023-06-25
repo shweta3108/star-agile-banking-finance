2895561af4b2eb5b13b313a0a5f18ab74e73a287
shweta3108 <60587685+shweta3108@users.noreply.github.com>
2023-06-25 07:46:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "Prod-Server" {
  ami = "ami-0261755bbcb8c4a84"
  instance_type = "t2.micro"
- availability_zone = "us-east-1"
+ availability_zone = "us-east-1a"
  key_name = "devops"
 
 tags ={
