2a583b0d8292ea29cfac38071711f30e789acef5
shweta3108 <60587685+shweta3108@users.noreply.github.com>
2023-06-26 15:15:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -142,7 +142,7 @@ resource "aws_instance" "proj-instance" {
      apt-get update -y
   apt install docker.io -y
   systemctl enable docker
-  docker run -itd -p 8087:8081 shwetas27/finance-me:1.0
+  docker run -itd -p 8084:8081 shwetas27/finance-me:1.0
   docker start $(docker ps -aq)
   docker ps
 tags = {
