97d018c665bef05667b66c76098f181517700f34
shweta3108 <60587685+shweta3108@users.noreply.github.com>
2023-06-26 15:23:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -139,12 +139,11 @@ resource "aws_instance" "proj-instance" {
   
 
   user_data = <<EOF
-     apt-get update -y
-  apt install docker.io -y
-  systemctl enable docker
-  docker run -itd -p 8084:8081 shwetas27/finance-me:1.0
-  docker start $(docker ps -aq)
-  docker ps
+    sudo apt update -y
+    sudo apt install nginx -y
+    sudo systemctl start nginx
+     sudo systemctl enable nginx
+
 tags = {
       Name = "terraform-instance"
 EOF
